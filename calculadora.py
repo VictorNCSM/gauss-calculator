03c63ee986f870d4e06742d2d47ff4a6a4d4aa1c
VictorNCSM <87883923+VictorNCSM@users.noreply.github.com>
2021-11-07 14:19:27 -0300

    Update calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -33,7 +33,7 @@ def valid(string):
         return False
 
 
-string = ["\nVocê sabia?\n\nEspelho convexo\nAs suas vantagens: Ele amplia o campo de visão, por isso é usado por motoristas de ônibus; ele dá mais visão, porém a imagem é pequena.\n===============================================================================================================\nEspelho côncavo:\nDeixa a imagem maior, e dependendo da distância podemos ver a imagem invertida.\n","\nVocê sabia? \n\nOs raios notáveis descritos são um artifício que facilita a determinação das características da imagem conjugada pelo espelho côncavo. Seu uso deve-se ao fato de que quaisquer raios de luz que incidam em superfícies refletoras côncavas serão refletidos assim como os raios notáveis o são.","\nVocê sabia?\n\nOs primeiros espelhos surgiram entre 5000 e 3000 A.C, não se sabe o lugar exato, alguns dizem ser na Suméria, porém as imagens do espelho não eram nítidas, porque era usado o bronze para produzi-las. Foi a partir do séc. 14, na Itália, que foram feitos os primeiros espelhos de vidro, após  ser realizada uma mistura de estanho e mercúrio. Todavia, muitas vezes os artesãos acabavam se contaminando, devido ao fato de que o mercúrio é um material extremamente poluente, e foi somente no século 19 que uma nova técnica foi desenvolvida, usando agora a prata química, ao invés do mercúrio."]
+string = ["\nVocê sabia?\n\nEspelho convexo\nAs suas vantagens: Ele amplia o campo de visão, por isso é usado por motoristas de ônibus; ele dá mais visão, porém a imagem é pequena.\n=============================================================================================\nEspelho côncavo:\nDeixa a imagem maior, e dependendo da distância podemos ver a imagem invertida.\n","\nVocê sabia? \n\nOs raios notáveis descritos são um artifício que facilita a determinação das características da imagem conjugada pelo espelho côncavo. Seu uso deve-se ao fato de que quaisquer raios de luz que incidam em superfícies refletoras côncavas serão refletidos assim como os raios notáveis o são.","\nVocê sabia?\n\nOs primeiros espelhos surgiram entre 5000 e 3000 A.C, não se sabe o lugar exato, alguns dizem ser na Suméria, porém as imagens do espelho não eram nítidas, porque era usado o bronze para produzi-las. Foi a partir do séc. 14, na Itália, que foram feitos os primeiros espelhos de vidro, após  ser realizada uma mistura de estanho e mercúrio. Todavia, muitas vezes os artesãos acabavam se contaminando, devido ao fato de que o mercúrio é um material extremamente poluente, e foi somente no século 19 que uma nova técnica foi desenvolvida, usando agora a prata química, ao invés do mercúrio."]
 
 
 var = input("Digite a variável que você precisa saber: ")
@@ -45,7 +45,7 @@ while not passagem:
     print('Caso queira saber o valor de f, insira "f" ou "F" (sem aspas) para que possamos coletar;')
     print('Caso queira saber o valor de p, insira "p" ou "P" (sem aspas) para que possamos coletar;')
     print('E por fim, caso queira saber o valor de p\', insira "p\'" ou "P\'" (sem aspas) para que possamos coletar.')
-    print("=========================================================================\n")
+    print("=============================================================================================\n")
     var = input("Digite a variável que você precisa saber: ")
     passagem = var in valido
 
@@ -53,13 +53,13 @@ while not passagem:
 if var == "f" or var == "F":
     p = input("Insira o valor de p: ")
     plinha = input("Insira o valor de p': ")
-    print("=========================================================================")
+    print("=============================================================================================")
 
     while valid(p) == False or valid(plinha) == False:
         print("Os valores que você inseriu estão incorretos. Por gentileza, insira valores numéricos.")
         p = input("Digite o valor de p: ")
         plinha = input("Digite o valor de p': ")
-        print("=========================================================================")
+        print("=============================================================================================")
     else:
         p = float(p)
         plinha = float(plinha)
@@ -76,7 +76,7 @@ if var == "f" or var == "F":
             f = p
 
         print(f"O valor de f é igual a: {f:.2f}")
-        print("=========================================================================\n")
+        print("==============================================================================================\n")
         print("\n", string[randint(0, 2)])
         input("Pressione enter para sair.")
         
@@ -89,7 +89,7 @@ if var == "f" or var == "F":
         f = denominador / numerador
 
         print(f"O valor de f é igual a: {f:.2f}")
-        print("=========================================================================\n")
+        print("=============================================================================================\n")
         print("\n", string[randint(0, 2)])
         input("Pressione enter para sair.")
         
@@ -99,7 +99,7 @@ if var == "f" or var == "F":
         f = denominador / numerador
 
         print(f"O valor de f é igual a: {f:.2f}")
-        print("=========================================================================\n")
+        print("=============================================================================================\n")
         print("\n", string[randint(0, 2)])
         input("Pressione enter para sair.")
 
@@ -107,13 +107,13 @@ elif var == "p" or var == "P":
 
     f = input("Insira o valor de f: ")
     plinha = input("Insira o valor de p': ")
-    print("=========================================================================")
+    print("=============================================================================================")
 
     while valid(f) == False or valid(plinha) == False:
         print("Os valores que você inseriu estão incorretos. Por gentileza, insira valores numéricos.")
         f = input("Digite o valor de f: ")
         plinha = input("Digite o valor de p': ")
-        print("=========================================================================")
+        print("=============================================================================================")
     else:
         f = float(f)
         plinha = float(plinha)
@@ -131,7 +131,7 @@ elif var == "p" or var == "P":
             p = f
 
         print(f"O valor de p é igual a: {p:.2f}")
-        print("=========================================================================\n")
+        print("=============================================================================================\n")
         print("\n", string[randint(0, 2)])
         input("Pressione enter para sair.")
         
@@ -139,7 +139,7 @@ elif var == "p" or var == "P":
 
         p = 0
         print(f"O valor de p é igual a: {p:.2f}")
-        print("=========================================================================\n")
+        print("=============================================================================================\n")
         print("\n", string[randint(0, 2)])
         input("Pressione enter para sair.")
 
@@ -156,7 +156,7 @@ elif var == "p" or var == "P":
 elif var == "p'" or var == "P'":
     f = input("Insira o valor de f: ")
     p = input("Insira o valor de p: ")
-    print("=========================================================================\n")
+    print("==============================================================================================\n")
 
     while valid(f) == False or valid(p) == False:
         print("Os valores que você inseriu estão incorretos. Por gentileza, insira valores numéricos.")
@@ -176,7 +176,7 @@ elif var == "p'" or var == "P'":
             plinha = p - (p * 2)
             
         print(f"O valor de p' é igual a: {plinha:.2f}")
-        print("=========================================================================\n")
+        print("=============================================================================================\n")
         print("\n", string[randint(0, 2)])
         input("Pressione enter para sair.")
 
